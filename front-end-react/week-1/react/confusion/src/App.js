b9867932fd278cd1d501c69cc9d072ec11ada81e
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2022-04-29 00:59:13 -0700

    Components Part 1

--- front-end-react/week-1/react/confusion/src/App.js
+++ front-end-react/week-1/react/confusion/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { Menuone } from './components/MenuoneComponent';
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Navbar dark color='primary'>
         <div className='container'>
           <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
+          <Menuone/>
         </div>
       </Navbar>
     </div>
